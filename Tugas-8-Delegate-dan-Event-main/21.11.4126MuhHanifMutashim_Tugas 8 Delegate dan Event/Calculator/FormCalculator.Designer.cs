f587e188fd35f1a7711aed03e6dbe0466d5bf465
mhanifmutashim <119776102+mhanifmutashim@users.noreply.github.com>
2022-12-06 18:03:11 +0700

    Add files via upload

---
+++ Tugas-8-Delegate-dan-Event-main/21.11.4126MuhHanifMutashim_Tugas 8 Delegate dan Event/Calculator/FormCalculator.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Calculator
+{
+    partial class FormCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnProses = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbOperasi = new System.Windows.Forms.ComboBox();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bttnProses
+            // 
+            this.bttnProses.Location = new System.Drawing.Point(144, 234);
+            this.bttnProses.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bttnProses.Name = "bttnProses";
+            this.bttnProses.Size = new System.Drawing.Size(136, 29);
+            this.bttnProses.TabIndex = 0;
+            this.bttnProses.Text = "Proses";
+            this.bttnProses.UseVisualStyleBackColor = true;
+            this.bttnProses.Click += new System.EventHandler(this.bttnProses_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Operasi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nilai B";
+            // 
+            // cmbOperasi
+            // 
+            this.cmbOperasi.FormattingEnabled = true;
+            this.cmbOperasi.Items.AddRange(new object[] {
+            "Penjumlahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.cmbOperasi.Location = new System.Drawing.Point(144, 61);
+            this.cmbOperasi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbOperasi.Name = "cmbOperasi";
+            this.cmbOperasi.Size = new System.Drawing.Size(136, 28);
+            this.cmbOperasi.TabIndex = 5;
+            this.cmbOperasi.SelectedIndexChanged += new System.EventHandler(this.cmbOperasi_SelectedIndexChanged);
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(144, 120);
+            this.txtNilaiA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(136, 26);
+            this.txtNilaiA.TabIndex = 6;
+            this.txtNilaiA.TextChanged += new System.EventHandler(this.txtNilaiA_TextChanged);
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(144, 178);
+            this.txtNilaiB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(136, 26);
+            this.txtNilaiB.TabIndex = 7;
+            this.txtNilaiB.TextChanged += new System.EventHandler(this.txtNilaiB_TextChanged);
+            // 
+            // FormCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 330);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.cmbOperasi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttnProses);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormCalculator";
+            this.Text = "FormCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnProses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbOperasi;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+    }
+}
